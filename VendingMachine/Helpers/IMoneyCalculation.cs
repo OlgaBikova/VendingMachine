a92c3aa7b1d5681d636716ed9896e4b12c2babaf
Olga Bikova <olgabikova@inbox.lv>
2015-04-28 00:18:09 +0300

    Add interface for money calculation

---
+++ VendingMachine/Helpers/IMoneyCalculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Helpers
+{
+    public interface IMoneyCalculation
+    {
+        Money Add(Money amount, Money currentMoney);
+
+        Money Subtract(Money amount, Money currentMoney);
+    }
+}
